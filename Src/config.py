f88edda029946a1b17498e47ea2adc0ddd9f10ef
Rizky Sugiharto <rizkysugiharto132@gmail.com>
2025-05-29 16:04:58 +0700

    Add connection pooling features

--- Src/config.py
+++ Src/config.py
@@ -1,2 +1,5 @@
 MYSQL_URL='mysql://uccm6gvknzcljmve:jOjNBAl1FKJq6bR803JU@bozxdhwsyd3xjc57debm-mysql.services.clever-cloud.com:3306/bozxdhwsyd3xjc57debm'
+MYSQL_POOL_SIZE=3
+MYSQL_MAX_OVERFLOW=2
+MYSQL_POOL_RECYCLE=300 # 3 minutes
 JWT_SECRET_KEY=''
--- Src/database.py
+++ Src/database.py
@@ -6,7 +6,12 @@ from typing import Annotated
 from Src import config
 
 pymysql.install_as_MySQLdb()
-engine = create_engine(config.MYSQL_URL)
+engine = create_engine(
+    config.MYSQL_URL,
+    pool_size=config.MYSQL_POOL_SIZE,
+    max_overflow=config.MYSQL_MAX_OVERFLOW,
+    pool_recycle=config.MYSQL_POOL_RECYCLE
+)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
